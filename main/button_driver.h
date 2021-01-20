5f3260c27a54f9f3f3bd5920ce3c9d22f500c1e1
nic8854@gmail.com <nic8854@gmail.com>
2021-01-20 14:05:38 +0100

    Update button_driver.h

--- main/button_driver.h
+++ main/button_driver.h
@@ -1,5 +1,15 @@
 #ifndef MAIN_BUTTON_DRIVER_H_
 #define MAIN_BUTTON_DRIVER_H_
 
+static esp_err_t read_reg_8(expander_t *dev, uint8_t reg, uint16_t *val);
+static esp_err_t write_reg_8(expander_t *dev, uint8_t reg, uint16_t val);
+
+typedef struct
+{
+    i2c_dev_t i2c_dev;
+
+    uint16_t config;
+    float i_lsb, p_lsb;
+} expander_t;
 
 #endif
