f2512144230848fed63f0a57a680bd98bc4b02ea
nic8854@gmail.com <nic8854@gmail.com>
2021-03-22 14:29:16 +0100

    Update IO_driver.c

--- main/IO_driver.c
+++ main/IO_driver.c
@@ -10,6 +10,10 @@
 
 static const char *TAG = "IO_Driver";
 
+#define GPIO_OUTPUT_IO_0    2
+#define GPIO_OUTPUT_IO_1    26
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
+
 //temporary
 expander_t dev_port_expander;
 conf_t config = Default_Config;
@@ -46,7 +50,7 @@ void IO_handler(void *pvParameters)
 
 void IO_init(int I2C_PORT, int SDA_GPIO, int SCL_GPIO)
 {	
-	//temporary
+	//Expander Init
     memset(&dev_port_expander, 0, sizeof(expander_t));
 	config.conf_port_0 = 0xFF;
 	config.conf_port_1 = 0x00;
@@ -54,6 +58,15 @@ void IO_init(int I2C_PORT, int SDA_GPIO, int SCL_GPIO)
 	config.pol_inv_1 = 0x00;
     expander_init_desc(&dev_port_expander, expander_addr_low, I2C_PORT, SDA_GPIO, SCL_GPIO);
 	expander_configure(&dev_port_expander, &config);
+
+    //GPIO Init
+    gpio_config_t io_conf;
+	io_conf.intr_type = GPIO_INTR_DISABLE;
+	io_conf.mode = GPIO_MODE_OUTPUT;
+	io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+	io_conf.pull_down_en = 0;
+	io_conf.pull_up_en = 0;
+	gpio_config(&io_conf);
 	xIO_Semaphore = xSemaphoreCreateMutex();
 	xTaskCreate(IO_handler, "IO_handler", 1024*4, NULL, 2, NULL);
 }
